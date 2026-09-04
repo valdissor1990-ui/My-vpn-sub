fef9df20da58cb2f36d1e42e44571f4962d1d6be
valdissor1990-ui <valdissor1990@gmail.com>
2026-09-04 20:22:11 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Источники подписок
+SOURCES = [
+    "https://raw.githubusercontent.com/itsyebekhe/PSG/main/subscriptions/xray/mix.b64",
+    "https://raw.githubusercontent.com/itsyebekhe/PSG/main/subscriptions/xray/reality.b64",
+    "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/mix_base64",
+    "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/Sub_Merge.txt",
+    "https://raw.githubusercontent.com/Sarinaesmailzadeh/V2Hub/main/merged_base64",
+    "https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_base64_Sub.txt",
+    "https://raw.githubusercontent.com/ircfspace/tvc/main/sub/mix_base64",
+    "https://raw.githubusercontent.com/itsyebekhe/HiN-VPN/main/subscription/normal/mix_base64",
+]
+
+# Настройки тестирования
+PING_TIMEOUT = 2  # секунд на пинг
+MAX_PING = 2000   # максимальный пинг в ms
+MAX_WORKERS = 20  # параллельных проверок
+PROTOCOLS = ["vless", "vmess", "trojan", "ss"]
